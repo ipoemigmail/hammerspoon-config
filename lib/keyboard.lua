d3bdec1b398ca5e56899a10bc0c108f1016e8d48
ben.jung <ben.jung@benjungui-MacBookPro.local>
2021-01-20 11:08:04 +0900

    quarter size added #2

--- lib/keyboard.lua
+++ lib/keyboard.lua
@@ -21,92 +21,164 @@ K.keyDownEventTap = hs.eventtap.new({events.keyDown}, function(event)
     if (event:getKeyCode() == hs.keycodes.map["left"]) then
       leftKeyDown = true
       if (T.isBindKeyDown(event)) then
+        local currentAction = ""
         if lastAction == "left" then
           W.resizeQuarterWidth()
-          lastAction = ""
+          currentAction = ""
         else
           W.resizeHalfWidth()
-          lastAction = "left"
+          currentAction = "left"
         end
         W.resizeMaxHeight()
         W.locateLeft()
         W.locateTop()
         if (upKeyDown) then
-          W.resizeHalfHeight()
+          if lastAction == "left + up" then
+            W.resizeHalfHeight()
+            W.resizeQuarterWidth()
+            lastAction = ""
+          else
+            W.resizeHalfHeight()
+            lastAction = "left + up"
+          end
           C.printConsole("left + up")
         elseif (downKeyDown) then
-          W.resizeHalfHeight()
+          if lastAction == "left + down" then
+            W.resizeHalfHeight()
+            W.resizeQuarterWidth()
+            lastAction = ""
+          else
+            W.resizeHalfHeight()
+            lastAction = "left + down"
+          end
           W.locateDown()
           C.printConsole("left + down")
+        else
+          lastAction = currentAction
         end
         return true
       end
     elseif (event:getKeyCode() == hs.keycodes.map["right"]) then
       rightKeyDown = true
       if (T.isBindKeyDown(event)) then
+        local currentAction = ""
         if lastAction == "right" then
           W.resizeQuarterWidth()
-          lastAction = ""
+          currentAction = ""
         else
           W.resizeHalfWidth()
-          lastAction = "right"
+          currentAction = "right"
         end
         W.resizeMaxHeight()
         W.locateRight()
         W.locateTop()
         if (upKeyDown) then
-          W.resizeHalfHeight()
+          if lastAction == "right + up" then
+            W.resizeQuarterWidth()
+            W.resizeHalfHeight()
+            W.locateRight()
+            lastAction = ""
+          else
+            W.resizeHalfHeight()
+            lastAction = "right + up"
+          end
           C.printConsole("right + up")
         elseif (downKeyDown) then
-          W.resizeHalfHeight()
+          if lastAction == "right + down" then
+            W.resizeQuarterWidth()
+            W.resizeHalfHeight()
+            W.locateRight()
+            lastAction = ""
+          else
+            W.resizeHalfHeight()
+            lastAction = "right + down"
+          end
           W.locateDown()
           C.printConsole("right + down")
+        else
+          lastAction = currentAction
         end
         return true
       end
     elseif (event:getKeyCode() == hs.keycodes.map["up"]) then
       upKeyDown = true
       if (T.isBindKeyDown(event)) then
+        local currentAction = ""
         if lastAction == "up" then
           W.resizeQuarterHeight()
-          lastAction = ""
+          currentAction = ""
         else
           W.resizeHalfHeight()
-          lastAction = "up"
+          currentAction = "up"
         end
         W.resizeMaxWidth()
         W.locateTop()
         W.locateLeft()
         if (leftKeyDown) then
-          W.resizeHalfWidth()
-          C.printConsole("right + up")
-        elseif (rightKeyDown) then
-          W.resizeHalfWidth()
-          W.locateRight()
+          if lastAction == "left + up" then
+            W.resizeQuarterHeight()
+            W.resizeHalfWidth()
+            lastAction = ""
+          else
+            W.resizeHalfWidth()
+            lastAction = "left + up"
+          end
           C.printConsole("left + up")
+        elseif (rightKeyDown) then
+          if lastAction == "right + up" then
+            W.resizeQuarterHeight()
+            W.resizeHalfWidth()
+            lastAction = ""
+          else
+            W.resizeHalfWidth()
+            lastAction = "right + up"
+          end
+          W.locateRight()
+          C.printConsole("right + up")
+        else
+          lastAction = currentAction
         end
         return true
       end
     elseif (event:getKeyCode() == hs.keycodes.map["down"]) then
       downKeyDown = true
       if (T.isBindKeyDown(event)) then
+        local currentAction = ""
         if lastAction == "down" then
           W.resizeQuarterHeight()
-          lastAction = ""
+          currentAction = ""
         else
           W.resizeHalfHeight()
-          lastAction = "down"
+          currentAction = "down"
         end
         W.resizeMaxWidth()
         W.locateDown()
         W.locateLeft()
         if (leftKeyDown) then
-          W.resizeHalfWidth()
-          C.printConsole("right + down")
+          if lastAction == "left + down" then
+            W.resizeQuarterHeight()
+            W.resizeHalfWidth()
+            W.locateDown()
+            lastAction = ""
+          else
+            W.resizeHalfWidth()
+            lastAction = "left + down"
+          end
+          C.printConsole("left + down")
         elseif (rightKeyDown) then
-          W.resizeHalfWidth()
+          if lastAction == "right + down" then
+            W.resizeQuarterHeight()
+            W.resizeHalfWidth()
+            W.locateDown()
+            lastAction = ""
+          else
+            W.resizeHalfWidth()
+            lastAction = "right + down"
+          end
           W.locateRight()
           C.printConsole("left + down")
+        else
+          lastAction = currentAction
         end
         return true
       end
